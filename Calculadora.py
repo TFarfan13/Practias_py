19951999c45479e92a4455d99b48f50aae868fa3
TFarfan13 <168193263+TFarfan13@users.noreply.github.com>
2024-07-22 09:16:59 -0300

    Se agrega funcion calcular

---
+++ Calculadora.py
@@ -0,0 +1,32 @@
+print("Calculadora ")
+def calcular():
+    print("Introduzca 2 números, luego seleccione la operación.")
+    
+    try:
+        num1 = float(input("Ingrese el primer número: "))
+        num2 = float(input("Ingrese el segundo número: "))
+    except ValueError:
+        print("Por favor, ingrese números válidos.")
+        return
+
+    ope = input("Ingrese la operación a realizar con los signos (+) suma, (-) resta, (*) multiplicación, (/) división: ")
+
+    if ope == "+":
+        result = num1 + num2
+        print(f"Resultado de la suma: {result}")
+    elif ope == "-":
+        result = num1 - num2
+        print(f"Resultado de la resta: {result}")
+    elif ope == "*":
+        result = num1 * num2
+        print(f"Resultado de la multiplicación: {result}")
+    elif ope == "/":
+        if num2 != 0:
+            result = num1 / num2
+            print(f"Resultado de la división: {result}")
+        else:
+            print("No se puede dividir por cero.")
+    else:
+        print("Operación no válida. Inténtelo de nuevo.")    
+        
+
